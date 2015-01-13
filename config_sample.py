9d94ab6b562257a502480dd460a4eb339482e123
chinwendu <chinwendu@genua.fr>
2015-01-13 01:09:24 +0100

    A sample config file

---
+++ config_sample.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# mysql credentials
+mysql = {
+  "user": "piratebay",
+  "passwd": "PASSWORD",
+  "db":"piratebay",
+  "host":"localhost"
+}
+
+# crawler base port. port will be choosen (deterministically)
+# between crawler_base_port and crawler_base_port + 255
+crawler_base_port = 12345
+
+# power of 2 if the number of dht instance de lauch while crawling
+# zb 4 for 2^4=16 instances
+crawler_instance = 3  # 8 instances
+
+# max virtual memory in byte
+crawler_max_memory = 4 * 1024 * 1024 * 1024 # 4GB
