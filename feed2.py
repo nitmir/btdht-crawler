98fe46b81309361eab5d3cf0c3cec1429c0d1d9e
Valentin Samir <valentin.samir@crans.org>
2016-10-27 13:43:13 +0200

    Add argument to feed2.py, nice progress bars and loop functionnality

--- feed2.py
+++ feed2.py
@@ -7,6 +7,8 @@ import hashlib
 import chardet
 import functools
 import json
+import argparse
+import progressbar
 from bson.binary import Binary
 from btdht import utils
 
@@ -16,6 +18,10 @@ class TorrentNoName(ValueError):
     pass
 class TorrentFileBadPathType(ValueError):
     pass
+
+def widget(what=""):
+    padding = 30
+    return [progressbar.ETA(), ' ', progressbar.Bar('='), ' ', progressbar.SimpleProgress(), ' ' if what else "", what]
 
 @functools.total_ordering
 class TorrentFile(object):
@@ -90,12 +96,19 @@ class Torrent(object):
                encoding = "utf-8"
         else:
             if b'name' in torrent[b'info']:
-                encoding = chardet.detect(torrent[b'info'][b'name'])['encoding']
+                try:
+                    encoding = chardet.detect(torrent[b'info'][b'name'])['encoding']
+                except TypeError:
+                    torrent[b'info'][b'name'] = str(torrent[b'info'][b'name'])
+                    encoding = chardet.detect(torrent[b'info'][b'name'])['encoding']
             if not encoding:
                 encoding = "utf-8"
 
         if b'name.utf-8' in torrent[b'info']:
-            self.name = torrent[b'info'][b'name.utf-8'].decode("utf-8", 'ignore')
+            try:
+                self.name = torrent[b'info'][b'name.utf-8'].decode("utf-8", 'ignore')
+            except AttributeError:
+                self.name = str(torrent[b'info'][b'name.utf-8']).decode("utf-8", 'ignore')
         elif b'name' in torrent[b'info']:
             self.name = torrent[b'info'][b'name'].decode(encoding, 'ignore')
         else:
@@ -146,25 +159,49 @@ class Torrent(object):
 
 
 class Manager(object):
-    def __init__(self):
+
+    last_process = 0
+
+    def __init__(self, progress=False):
         self.db1 = pymongo.MongoClient()[config.mongo["db"]]["torrents"]
         self.db2 = pymongo.MongoClient()[config.mongo["db"]]["torrents_data"]
+        self.db3 = pymongo.MongoClient()[config.mongo["db"]]["torrents_stats"]
+        self.progress = progress
+
+    def add_stats(self, force=False):
+        last_stats = self.db3.find().sort([('_id', -1)]).limit(1).next()
+        if force or time.time() - last_stats['_id'] >= 1800:
+            hash_tracked = self.db1.find({"status":{"$in": [0, None]}}).count()
+            torrent_indexed = self.db2.find().count()
+            print "Record stats: trackings %s hash, indexed %s torrents" % (hash_tracked, torrent_indexed)
+            self.db3.insert({"_id": int(time.time()), "hash_tracked": hash_tracked, "torrent_indexed": torrent_indexed})
 
     def clean_db(self):
         sys.stdout.write("Deleting old torrents from db... ")
         one_hour_ago = int(time.time()) - 3600
-        result1 = self.db1.delete_many({"dht_last_get": {"$lt": one_hour_ago}, "status":{"$nin": [0, None]}})
-        result2 = self.db1.delete_many({"dht_last_announce": {"$lt": one_hour_ago}, "status":{"$nin": [0, None]}})
+        result1 = self.db1.delete_many({"dht_last_get": {"$lt": one_hour_ago}, "status":{"$in": [0, None]}})
+        result2 = self.db1.delete_many({"dht_last_announce": {"$lt": one_hour_ago}, "status":{"$in": [0, None]}})
         print "%s deleted" % (result1.deleted_count + result2.deleted_count)
 
     def process_new_torrents(self):
-        for file in os.listdir(config.torrents_dir):
+        i = 0
+        files = os.listdir(config.torrents_dir)
+        if self.progress:
+            pbar = progressbar.ProgressBar(widgets=widget("added new torrents"), maxval=len(files)).start()
+        else:
+            sys.stdout.write("Adding new torrents... ")
+            sys.stdout.flush()
+        for file in files:
             if file.endswith(".torrent"):
-                print file
+                i += 1
                 torrent_path = os.path.join(config.torrents_dir, file)
                 self._process_torrent(torrent_path)
-            else:
-                print "skip %s" % file
+            if self.progress:
+                pbar.update(pbar.currval + 1)
+        if self.progress:
+            pbar.finish()
+        else:
+            print "%s added" % i
 
     def _process_torrent(self, path):
         torrent = Torrent(path)
@@ -186,10 +223,50 @@ class Manager(object):
                             if path.endswith(".torrent"):
                                 print path
                                 self._process_torrent(path)
-                
+
+    def process_args(self, args):
+        self.last_process = int(time.time())
+        if args.add_new_torrents or args.all:
+            self.process_new_torrents()
+        if args.clean_db or args.all:
+            self.clean_db()
+        if args.record_stats or args.all:
+            self.add_stats(force=args.record_stats)
+        if args.reprocess_done_torrents:
+            self.reprocess_done_torrents()
+
+    def sleep(self, sleep):
+        sleep_time = int(max(0, sleep - (time.time() - self.last_process)))
+        if sleep_time > 0:
+            print("Now spleeping until the next loop")
+            if self.progress:
+                pbar = progressbar.ProgressBar(widgets=sleep_widget, maxval=sleep_time).start()
+                for i in xrange(sleep_time):
+                    time.sleep(1)
+                    pbar.update(i+1)
+                pbar.finish()
+            else:
+                time.sleep(sleep_time)
+
 
 if __name__ == "__main__":
-    manager = Manager()
-    manager.process_new_torrents()
-    manager.clean_db()
-    #manager.reprocess_done_torrents()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--progress", "-P", help="display a progress bar for each action", action="store_true")
+    parser.add_argument("--add-new-torrents", "-A", help="process new torrents and add them to the database", action="store_true")
+    parser.add_argument("--clean-db", "-D", help="Delete old hash from the database", action="store_true")
+    parser.add_argument("--record-stats", "-S", help="Add a stats record", action="store_true")
+    parser.add_argument("--reprocess-done-torrents", help="Try to readd to database already processed torrents", action="store_true")
+    parser.add_argument("--all", help="add new torrents, Delete old hash, and Add a stats record if needed. Kind of -A -D -S equivalent", action="store_true")
+    parser.add_argument("--loop", help="Loop actions every 5 minutes", action="store_true")
+    args = parser.parse_args()
+    manager = Manager(args.progress)
+    if not args.loop:
+        manager.process_args(args)
+    else:
+        sleep_widget = [progressbar.Bar('>'), ' ', progressbar.ETA(), ' ', progressbar.ReverseBar('<')]
+        while True:
+            try:
+                manager.process_args(args)
+            except pymongo.errors.PyMongoError as error:
+                print "PyMongoError: %s" %error
+            manager.sleep(60)
